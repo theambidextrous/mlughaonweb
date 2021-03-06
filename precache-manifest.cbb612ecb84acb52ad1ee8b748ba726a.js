9659d45e084e5d7aa582e899628046c582b3a8b2
Soft Bucket IO <softbucket.io@gmail.com>
2021-03-06 21:32:59 +0300

    mpesa access tkn fix

---
+++ precache-manifest.cbb612ecb84acb52ad1ee8b748ba726a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "af61443e90ec6e38601e",
+    "url": "https://apps.m-lugha.com/static/js/0.3aa71efd.chunk.js"
+  },
+  {
+    "revision": "8f1112b8cc44341dc631",
+    "url": "https://apps.m-lugha.com/static/css/main.f0b3691d.chunk.css"
+  },
+  {
+    "revision": "8f1112b8cc44341dc631",
+    "url": "https://apps.m-lugha.com/static/js/main.c3152575.chunk.js"
+  },
+  {
+    "revision": "694bf1420e09e9fb64dc",
+    "url": "https://apps.m-lugha.com/static/js/runtime~main.ca218e53.js"
+  },
+  {
+    "revision": "e9bfe38c6753d614eb87",
+    "url": "https://apps.m-lugha.com/static/css/3.0a91a0eb.chunk.css"
+  },
+  {
+    "revision": "e9bfe38c6753d614eb87",
+    "url": "https://apps.m-lugha.com/static/js/3.3de66868.chunk.js"
+  },
+  {
+    "revision": "ac84c2ffde114b1c5cfd",
+    "url": "https://apps.m-lugha.com/static/js/4.46ac3ce4.chunk.js"
+  },
+  {
+    "revision": "b90f497d29c497f5464f",
+    "url": "https://apps.m-lugha.com/static/js/5.0555591e.chunk.js"
+  },
+  {
+    "revision": "358bbd2e32b9705f11b1",
+    "url": "https://apps.m-lugha.com/static/js/6.ee1306ba.chunk.js"
+  },
+  {
+    "revision": "4d27320f877bdb9dc223",
+    "url": "https://apps.m-lugha.com/static/js/7.6d6e8972.chunk.js"
+  },
+  {
+    "revision": "f069126aa4a9b5f75d12",
+    "url": "https://apps.m-lugha.com/static/js/8.7b0d816e.chunk.js"
+  },
+  {
+    "revision": "0c32b1045ecd50bd142f",
+    "url": "https://apps.m-lugha.com/static/js/9.fdf848d9.chunk.js"
+  },
+  {
+    "revision": "01b768d617a0242bcb387df9e9f9c16f",
+    "url": "https://apps.m-lugha.com/static/media/logo_200.01b768d6.png"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://apps.m-lugha.com/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "4d6fffea2abbc5828877e800e1c640e0",
+    "url": "https://apps.m-lugha.com/static/media/100_1.4d6fffea.png"
+  },
+  {
+    "revision": "1e2a29d9f97df3738e797c90062be0e4",
+    "url": "https://apps.m-lugha.com/static/media/logo_200.1e2a29d9.gif"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "https://apps.m-lugha.com/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "deb53f884011a18b465d149fd5a5966a",
+    "url": "https://apps.m-lugha.com/static/media/123.deb53f88.jpg"
+  },
+  {
+    "revision": "5983db65e6042653f85b16481973be0b",
+    "url": "https://apps.m-lugha.com/static/media/abc.5983db65.jpg"
+  },
+  {
+    "revision": "acfe7046f47d96b9cb598622e50fe610",
+    "url": "https://apps.m-lugha.com/static/media/background_1920-18.acfe7046.jpg"
+  },
+  {
+    "revision": "1420d15b22ed3f39abc468212d735814",
+    "url": "https://apps.m-lugha.com/static/media/background_640-1.1420d15b.jpg"
+  },
+  {
+    "revision": "55e47e98ef3c84cf5eb4706a4c74311a",
+    "url": "https://apps.m-lugha.com/static/media/background_640-3.55e47e98.jpg"
+  },
+  {
+    "revision": "b7f68e8afaec5bfad8fdcc44e4e4e483",
+    "url": "https://apps.m-lugha.com/static/media/background_1920-11.b7f68e8a.jpg"
+  },
+  {
+    "revision": "d214758e249c95f42995fc8cb997a604",
+    "url": "https://apps.m-lugha.com/static/media/poster.d214758e.jpg"
+  },
+  {
+    "revision": "f60476950114ee7e7d52adbfc79576d8",
+    "url": "https://apps.m-lugha.com/static/media/landing-2-image.f6047695.png"
+  },
+  {
+    "revision": "ed782778fe033b91f62f5c05a62aef69",
+    "url": "https://apps.m-lugha.com/static/media/poster_img_one.ed782778.jpg"
+  },
+  {
+    "revision": "b9539e9228b82933ba726ad3fdeaac2e",
+    "url": "https://apps.m-lugha.com/static/media/poster_img_two.b9539e92.jpg"
+  },
+  {
+    "revision": "e1406f61ca46bb887ff8f9480b01c71d",
+    "url": "https://apps.m-lugha.com/static/media/landing-img.e1406f61.png"
+  },
+  {
+    "revision": "318610ad5595dcd808f85341147f3ff3",
+    "url": "https://apps.m-lugha.com/static/media/env.318610ad.jpg"
+  },
+  {
+    "revision": "904f2d17c96926d90ecbd47bc4466f95",
+    "url": "https://apps.m-lugha.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://apps.m-lugha.com/precache-manifest.a48cbc8217a32f340a95c1ab06ad4cf6.js"
+  "https://apps.m-lugha.com/precache-manifest.cbb612ecb84acb52ad1ee8b748ba726a.js"
 );
 
 workbox.clientsClaim();
